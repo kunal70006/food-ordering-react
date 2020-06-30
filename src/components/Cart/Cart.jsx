02f25d24d8027f62ce394a5128e35730c6807aff
Kunal <singhkunaldeep@gmail.com>
2020-06-30 17:09:35 +0530

    setting up

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div>
+      <main>
+        <h1>Hello from Cart</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <h1>Hello from home</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
